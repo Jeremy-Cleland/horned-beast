31dc1401745620de555ee704488a7419440e66d8
Jeremy Cleland <jeremy@clelandco.com>
2023-01-10 17:27:27 -0500

    add bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 import "../styles/Header.css";
 
 class Header extends React.Component {
   render() {
     return (
-      <header>
-        <h1>Horned Beast Gallary</h1>
-      </header>
+      <Navbar bg='dark' sticky='top'>
+        <Container>
+          <Navbar.Brand as={"h1"} className='text-white'>
+            Horned Beast
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
     );
   }
 }
--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import "../styles/HornedBeast.css";
 import Card from "react-bootstrap/Card";
 
@@ -19,9 +18,10 @@ class HornedBeast extends React.Component {
 
   render() {
     return (
-      <Card bg='secondary' style={{ width: "60rem" }}>
-        <Card.Title className='text-white'>{this.props.title}</Card.Title>
+      <Card className='bg-dark text-white' style={{ width: "60rem" }}>
+        <Card.Header as='h2'>{this.props.title}</Card.Header>
         <Card.Img
+          onClick={this.handleClick}
           className='text-dark'
           src={this.props.image_url}
           title={this.props.title}
@@ -31,10 +31,7 @@ class HornedBeast extends React.Component {
           <p>Description: {this.props.description}</p>
           <p>Horns: {this.props.horns}</p>
         </Card.Body>
-        <div className='text-white'>
-          {this.state.likes} ♥️
-          <Button onClick={this.handleClick}>♥️ Me!</Button>
-        </div>
+        <div className='text-white'>{this.state.likes} ♥️</div>
       </Card>
     );
   }
