230e6284bfcc9a8105a5541311d0ba1284d375dd
Jeremy Cleland <jeremy@clelandco.com>
2023-01-11 12:32:34 -0500

    updated CSS to make responsive

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "../styles/HornedBeast.css";
 import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 class HornedBeast extends React.Component {
   constructor(props) {
@@ -18,21 +21,27 @@ class HornedBeast extends React.Component {
 
   render() {
     return (
-      <Card className='bg-dark text-white' style={{ width: "60rem" }}>
-        <Card.Header as='h2'>{this.props.title}</Card.Header>
-        <Card.Img
-          onClick={this.handleClick}
-          className='text-dark'
-          src={this.props.image_url}
-          title={this.props.title}
-          alt={this.props.alt}
-        />
-        <Card.Body className='text-white'>
-          <p>Description: {this.props.description}</p>
-          <p>Horns: {this.props.horns}</p>
-        </Card.Body>
-        <div className='text-white'>{this.state.likes} ♥️</div>
-      </Card>
+      <Container fluid='xxl'>
+        <Row>
+          <Col>
+            <Card className='bg-dark text-white'>
+              <Card.Header as='h2'>{this.props.title}</Card.Header>
+              <Card.Img
+                onClick={this.handleClick}
+                className='text-dark'
+                src={this.props.image_url}
+                title={this.props.title}
+                alt={this.props.alt}
+              />
+              <Card.Body className='text-white'>
+                <p>Description: {this.props.description}</p>
+                <p>Horns: {this.props.horns}</p>
+              </Card.Body>
+              <div className='text-white'>{this.state.likes} ♥️</div>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
